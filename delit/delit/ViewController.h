0e9a97667203db76f6bf31bea82b9c0d0d29ce5c
Sarmad Zafar <szafar@soundviewtech.com>
2012-03-03 15:06:23 -0500

    ist ver

---
+++ delit/delit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delit
+//
+//  Created by User Account on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+    
+}
+
+@end
